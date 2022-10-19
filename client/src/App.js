3a994c0f294830ce9e8b2be225f4976dc9236c26
Artur0705 <artlil420@gmail.com>
2022-10-19 21:52:01 +1100

    Added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React, { useRef } from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Box, Flex, useDisclosure } from "@chakra-ui/react";
 
 import Hero from "./components/Hero";
 import Nav from "./components/Nav";
-import { useSelector } from "react-redux";
-
-import { Box, Flex, useDisclosure } from "@chakra-ui/react";
+import ContactUs from "./components/ContactUs";
+import HomePage from "./pages/HomePage";
+import SigninPage from "./pages/SigninPage";
+import RegisterPage from "./pages/RegisterPage";
+import ProfilePage from "./pages/ProfilePage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   const userSignin = useSelector((state) => state.userSignin);
@@ -12,14 +23,29 @@ function App() {
   const { onOpen } = useDisclosure();
   const btnRef = useRef();
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route path="/" element={<HomePage />}></Route>,
+        <Route path="login" element={<SigninPage />}></Route>,
+        <Route path="register" element={<RegisterPage />}></Route>,
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    )
+  );
+
   return (
     <Box>
       <Nav ref={btnRef} onOpen={onOpen} userInfo={userInfo} />
+      <RouterProvider router={router} />
+
       <Flex>
         <Hero />
         <Hero />
         <Hero />
       </Flex>
+      <ContactUs />
     </Box>
   );
 }
