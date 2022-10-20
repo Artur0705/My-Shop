b6b87c33c07049c75f5ebf46575de7cd56d4bf4b
Artur0705 <artlil420@gmail.com>
2022-10-20 22:08:17 +1100

    Added order reducers

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.json());
 
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
 
 app.use(express.static(path.join(__dirname, "/../client/build")));
 
