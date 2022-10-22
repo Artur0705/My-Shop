bc1f7ef583ba09708bafe306aeea02a68e27dcbb
Artur0705 <artlil420@gmail.com>
2022-10-22 20:09:30 +1100

    Added page for shipping

---
+++ client/src/pages/ShippingPage.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { saveShipping } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
+import {
+  Button,
+  Container,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+
+function ShippingPage(props) {
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+  const [country, setCountry] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShipping({ address, city, postalCode, country }));
+    navigate("/payment");
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+
+      <Container maxW="550px">
+        <form onSubmit={submitHandler}>
+          <Text fontSize="4xl" textAlign={"center"}>
+            Shipping
+          </Text>
+
+          <FormControl>
+            <FormLabel>Address</FormLabel>
+            <Input
+              type="text"
+              name="address"
+              id="address"
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>City</FormLabel>
+            <Input
+              type="text"
+              name="city"
+              id="city"
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Postal Code</FormLabel>
+            <Input
+              type="text"
+              name="postalCode"
+              id="postalCode"
+              onChange={(e) => setPostalCode(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel>Country</FormLabel>
+            <Input
+              type="text"
+              name="country"
+              id="country"
+              onChange={(e) => setCountry(e.target.value)}
+            />
+          </FormControl>
+
+          <Flex alignItems={"center"} justifyContent={"space-between"}>
+            <Button type="submit" colorScheme="blue" mt="4">
+              Continue
+            </Button>
+          </Flex>
+        </form>
+      </Container>
+    </div>
+  );
+}
+export default ShippingPage;
