cf2707f94ad9f3204c97e4092493a62fc340a571
Artur0705 <artlil420@gmail.com>
2022-10-22 20:07:01 +1100

    Added page for all products

---
+++ client/src/pages/ProductsPage.js
@@ -0,0 +1,296 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import axios from "axios";
+import {
+  saveProduct,
+  listProducts,
+  deleteProdcut,
+} from "../actions/productActions";
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Textarea,
+  Th,
+  Thead,
+  Tr,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+function ProductsPage(props) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [brand, setBrand] = useState("");
+  const [category, setCategory] = useState("");
+  const [countInStock, setCountInStock] = useState("");
+  const [description, setDescription] = useState("");
+  const [uploading, setUploading] = useState(false);
+  const productList = useSelector((state) => state.productList);
+  const { loading, products, error } = productList;
+
+  const productSave = useSelector((state) => state.productSave);
+  const {
+    loading: loadingSave,
+    success: successSave,
+    error: errorSave,
+  } = productSave;
+
+  const productDelete = useSelector((state) => state.productDelete);
+  const {
+    loading: loadingDelete,
+    success: successDelete,
+    error: errorDelete,
+  } = productDelete;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (successSave) {
+      setModalVisible(false);
+    }
+    dispatch(listProducts());
+    return () => {
+      //
+    };
+  }, [successSave, successDelete]);
+
+  const openModal = (product) => {
+    setModalVisible(true);
+    setId(product._id);
+    setName(product.name);
+    setPrice(product.price);
+    setDescription(product.description);
+    setImage(product.image);
+    setBrand(product.brand);
+    setCategory(product.category);
+    setCountInStock(product.countInStock);
+    onOpen();
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      saveProduct({
+        _id: id,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description,
+      })
+    );
+  };
+  const deleteHandler = (product) => {
+    dispatch(deleteProdcut(product._id));
+  };
+  const uploadFileHandler = (e) => {
+    const file = e.target.files[0];
+    const bodyFormData = new FormData();
+    bodyFormData.append("image", file);
+    setUploading(true);
+    axios
+      .post("/api/uploads", bodyFormData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        setImage(response.data);
+        setUploading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setUploading(false);
+      });
+  };
+  return (
+    <div className="content content-margined">
+      <Container maxW="1120px">
+        <Text fontSize="4xl" textAlign={"center"}>
+          Products
+        </Text>
+
+        <TableContainer>
+          <Table variant="simple">
+            <TableCaption>
+              <Button onClick={() => openModal({})}>Create Product</Button>
+            </TableCaption>
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Price</Th>
+                <Th>Category</Th>
+                <Th>Brand</Th>
+                <Th>Action</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {products?.map((product) => (
+                <Tr key={product._id}>
+                  <Td>{product._id}</Td>
+                  <Td>{product.name}</Td>
+                  <Td>{product.price}</Td>
+                  <Td>{product.category}</Td>
+                  <Td>{product.brand}</Td>
+                  <Td>
+                    <button
+                      className="button"
+                      onClick={() => openModal(product)}
+                    >
+                      Edit
+                    </button>{" "}
+                    <button
+                      className="button"
+                      onClick={() => deleteHandler(product)}
+                    >
+                      Delete
+                    </button>
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Container>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create product</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {modalVisible && (
+              <div className="form">
+                <form onSubmit={submitHandler}>
+                  <div>
+                    {loadingSave && <div>Loading...</div>}
+                    {errorSave && <div>{errorSave}</div>}
+                  </div>
+
+                  <FormControl>
+                    <FormLabel>Name</FormLabel>
+                    <Input
+                      type="text"
+                      name="name"
+                      value={name}
+                      id="name"
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Price</FormLabel>
+                    <Input
+                      type="text"
+                      name="price"
+                      value={price}
+                      id="price"
+                      onChange={(e) => setPrice(e.target.value)}
+                    />
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Image</FormLabel>
+                    <Input
+                      type="text"
+                      name="image"
+                      value={image}
+                      id="image"
+                      onChange={(e) => setImage(e.target.value)}
+                    />
+                    <input type="file" onChange={uploadFileHandler}></input>
+                    {uploading && <div>Uploading...</div>}
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Brand</FormLabel>
+                    <Input
+                      type="text"
+                      name="brand"
+                      value={brand}
+                      id="brand"
+                      onChange={(e) => setBrand(e.target.value)}
+                    />
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>CountInStock</FormLabel>
+                    <NumberInput
+                      maxW="100px"
+                      mr="2rem"
+                      value={countInStock}
+                      onChange={(e) => setCountInStock(e.target.value)}
+                    >
+                      <NumberInputField />
+                      <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                      </NumberInputStepper>
+                    </NumberInput>
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Category</FormLabel>
+                    <Input
+                      type="text"
+                      name="category"
+                      value={category}
+                      id="category"
+                      onChange={(e) => setCategory(e.target.value)}
+                    />
+                  </FormControl>
+
+                  <FormControl>
+                    <FormLabel>Description</FormLabel>
+                    <Textarea
+                      name="description"
+                      value={description}
+                      id="description"
+                      onChange={(e) => setDescription(e.target.value)}
+                    ></Textarea>
+                  </FormControl>
+                </form>
+              </div>
+            )}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button type={"submit"} colorScheme="red">
+              {id ? "Update" : "Create"}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+}
+
+export default ProductsPage;
