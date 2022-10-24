c73b46b0435b17fcfc8a70846e3c56d695f95307
Artur0705 <artlil420@gmail.com>
2022-10-24 17:03:03 +1100

    Updated error page

--- client/src/pages/ErrorPage.js
+++ client/src/pages/ErrorPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./ErrorPage.css";
 
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className="my-container">
+        <div className="content">
+          <h4>Opps! Page not found</h4>
+          <p>The page you were looking for doesn't exist.</p>
+          <Link to="/">Back to home</Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
