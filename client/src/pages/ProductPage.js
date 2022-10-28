ace7b40a644f4c56f4a537b541f6ee0670abba4f
Artur0705 <artlil420@gmail.com>
2022-10-28 13:56:45 +1100

    Added comment and review section for products

--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -15,9 +15,17 @@ import {
   Tbody,
   Tr,
   Td,
+  Textarea,
+  FormControl,
+  FormLabel,
+  Select,
+  Container,
+  Thead,
+  Th,
 } from "@chakra-ui/react";
 import { PRODUCT_REVIEW_SAVE_RESET } from "../constants/productConstants";
 import Services from "../components/Services";
+import Rating from "../components/Rating";
 
 function ProductPage(props) {
   const [isLargerThanLG] = useMediaQuery("(min-width: 62em)");
@@ -33,7 +41,6 @@ function ProductPage(props) {
   const dispatch = useDispatch();
   const params = useParams();
   const navigate = useNavigate();
-  console.log(productSaveSuccess);
 
   useEffect(() => {
     if (productSaveSuccess) {
@@ -63,62 +70,133 @@ function ProductPage(props) {
 
   return (
     <>
-      {product && (
-        <Flex
-          width="full"
-          minHeight="70vh"
-          alignItems="center"
-          px={isLargerThanLG ? "16" : "6"}
-          py="16"
-          justifyContent="center"
-          flexDirection={isLargerThanLG ? "row" : "column"}
-        >
+      <Container maxW="550px">
+        {product && (
           <Flex
-            w={isLargerThanLG ? "40%" : "full"}
-            mb={isLargerThanLG ? "0" : "6"}
+            width="full"
+            minHeight="70vh"
             alignItems="center"
+            px={isLargerThanLG ? "16" : "6"}
+            py="16"
             justifyContent="center"
+            flexDirection={isLargerThanLG ? "row" : "column"}
           >
-            <Image src={product.image} alt={product.image} w="full" />
+            <Flex
+              w={isLargerThanLG ? "40%" : "full"}
+              mb={isLargerThanLG ? "0" : "6"}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Image src={product.image} alt={product.image} w="full" />
+            </Flex>
+            <Spacer />
+            <Flex
+              w={"60%"}
+              flexDirection="column"
+              ml={isLargerThanLG ? "7" : "0"}
+            >
+              <Text fontSize={isLargerThanLG ? "5xl" : "4xl"} fontWeight="bold">
+                {product.name}
+              </Text>
+
+              <Text mb="6" opacity="0.8">
+                {product.description}
+              </Text>
+
+              <TableContainer>
+                <Table variant="simple">
+                  <Tbody>
+                    <Tr>
+                      <Td>Price</Td>
+                      <Td>{product.price}$</Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
+
+              {product.countInStock > 0 && (
+                <Button
+                  width="200px"
+                  onClick={handleAddToCart}
+                  size="lg"
+                  colorScheme="blue"
+                >
+                  Add to Cart
+                </Button>
+              )}
+            </Flex>
           </Flex>
-          <Spacer />
-          <Flex
-            w={"60%"}
-            flexDirection="column"
-            ml={isLargerThanLG ? "7" : "0"}
-          >
-            <Text fontSize={isLargerThanLG ? "5xl" : "4xl"} fontWeight="bold">
-              {product.name}
-            </Text>
+        )}
+        <Container maxW="1120px">
+          <Text fontSize="4xl" textAlign={"center"}>
+            Review and Comment
+          </Text>
 
-            <Text mb="6" opacity="0.8">
-              {product.description}
-            </Text>
+          <TableContainer>
+            <Table variant="simple">
+              <Tbody>
+                {product &&
+                  product.reviews.map((review) => (
+                    <Tr key={review._id}>
+                      <Td>{review.name}</Td>
+                      <Td>
+                        <Rating value={review.rating}></Rating>
+                      </Td>
+                      <Td>{review.createdAt.substring(0, 10)}</Td>
+                      <Td>{review.comment}</Td>
+                    </Tr>
+                  ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Container>
 
-            <TableContainer>
-              <Table variant="simple">
-                <Tbody>
-                  <Tr>
-                    <Td>Price</Td>
-                    <Td>{product.price}$</Td>
-                  </Tr>
-                </Tbody>
-              </Table>
-            </TableContainer>
-
-            {product.countInStock > 0 && (
-              <Button
-                width="200px"
-                onClick={handleAddToCart}
-                size="lg"
-                colorScheme="blue"
-              >
-                Add to Cart
-              </Button>
-            )}
-          </Flex>
-        </Flex>
-      )}
+        <li>
+          <h3>Write a customer review</h3>
+          {userInfo ? (
+            <form onSubmit={submitHandler}>
+              <div className="form-container">
+                <FormControl>
+                  <FormLabel>Rating</FormLabel>
+                  <Select
+                    placeholder="Rating"
+                    value={rating}
+                    onChange={(e) => setRating(e.target.value)}
+                  >
+                    <option value="1">1- Poor</option>
+                    <option value="2">2- Fair</option>
+                    <option value="3">3- Good</option>
+                    <option value="4">4- Very Good</option>
+                    <option value="5">5- Excelent</option>
+                  </Select>
+                </FormControl>
+                <Text mb="8px">Comment</Text>
+                <Textarea
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  placeholder="Comment"
+                  size="sm"
+                />
+                <div>
+                  <Button
+                    type="submit"
+                    width="200px"
+                    mt="20px"
+                    size="lg"
+                    colorScheme="blue"
+                  >
+                    Submit
+                  </Button>
+                </div>
+              </div>
+            </form>
+          ) : (
+            <div>
+              Please <Link to="/signin">Sign-in</Link> to write a review.
+            </div>
+          )}
+        </li>
+      </Container>
     </>
   );
 }
