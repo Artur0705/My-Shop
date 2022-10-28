521f2ccaa4f258b689cde5ac0dbfa5fbc9d3962e
Artur0705 <artlil420@gmail.com>
2022-10-28 17:35:51 +1100

    Fixed typo in server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(route);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../FrontEnd/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 const PORT = process.env.PORT || 5000;
