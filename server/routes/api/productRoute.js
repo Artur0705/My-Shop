498217fa77e7c2c9ae93042e4982194036eeecbf
Artur0705 <artlil420@gmail.com>
2022-10-30 20:47:20 +1100

    Updated product route

--- server/routes/api/productRoute.js
+++ server/routes/api/productRoute.js
@@ -22,7 +22,6 @@ router.get("/", async (req, res) => {
   const products = await Product.find({ ...category, ...searchKeyword }).sort(
     sortOrder
   );
-  console.log(category);
 
   res.send(products);
 });
@@ -89,24 +88,28 @@ router.delete("/:id", isAuth, isAdmin, async (req, res) => {
 });
 
 router.post("/", isAuth, isAdmin, async (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    price: req.body.price,
-    image: req.body.image,
-    brand: req.body.brand,
-    category: req.body.category,
-    countInStock: req.body.countInStock,
-    description: req.body.description,
-    rating: req.body.rating,
-    numReviews: req.body.numReviews,
-  });
-  const newProduct = await product.save();
-  if (newProduct) {
-    return res
-      .status(201)
-      .send({ message: "New Product Created", data: newProduct });
+  try {
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      image: req.body.image,
+      brand: req.body.brand,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      description: req.body.description,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+    });
+    const newProduct = await product.save();
+    if (newProduct) {
+      return res
+        .status(201)
+        .send({ message: "New Product Created", data: newProduct });
+    }
+    return res.status(500).send({ message: " Error in Creating Product." });
+  } catch (error) {
+    res.status(401).send({ message: error.message });
   }
-  return res.status(500).send({ message: " Error in Creating Product." });
 });
 
 module.exports = router;
