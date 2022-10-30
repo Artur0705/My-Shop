646de81b46b37050e77ab93ccf1554dd46837c43
Artur0705 <artlil420@gmail.com>
2022-10-30 12:12:13 +1100

    Updated Routes in App

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,23 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { Box, Flex, useDisclosure } from "@chakra-ui/react";
+import { useDisclosure, Box } from "@chakra-ui/react";
 
-import Hero from "./components/Hero";
 import Nav from "./components/Nav";
-import ContactUs from "./components/ContactUs";
-import AboutUs from "./components/AboutUs";
-import Testimonials from "./components/Testimonials";
-import Footer from "./components/Footer";
+import DrawerComponent from "./components/DrawerComponent";
 import HomePage from "./pages/HomePage";
-import SigninPage from "./pages/SigninPage.js";
+import ContactUs from "./components/ContactUs";
+import SigninPage from "./pages/SigninPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
-import ErrorPage from "./pages/ErrorPage";
-import DrawerComponent from "./components/DrawerComponent";
-import ProductsPage from "./pages/ProductsPage";
-import ShippingPage from "./pages/ShippingPage";
-import PlaceOrderPage from "./pages/PlaceOrderPage";
 import ProductPage from "./pages/ProductPage";
+import ProductsPage from "./pages/ProductsPage";
 import CartPage from "./pages/CartPage";
+import PaymentPage from "./pages/PaymentPage";
+import PlaceOrderPage from "./pages/PlaceOrderPage";
+import ShippingPage from "./pages/ShippingPage";
+import Footer from "./components/Footer";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   const userSignin = useSelector((state) => state.userSignin);
@@ -36,29 +34,26 @@ function App() {
     createRoutesFromElements(
       <Route path="/">
         <Route path="/" element={<HomePage />}></Route>,
-        <Route path="login" element={<SigninPage />}></Route>
+        <Route path="login" element={<SigninPage />}></Route>,
         <Route path="register" element={<RegisterPage />}></Route>,
         <Route path="profile" element={<ProfilePage />} />
         <Route path="products" element={<ProductsPage />} />
         <Route path="product/:id" element={<ProductPage />} />
         <Route path="shipping" element={<ShippingPage />} />
+        <Route path="payment" element={<PaymentPage />} />
         <Route path="placeorder" element={<PlaceOrderPage />} />
         <Route path="category/:id" element={<HomePage />} />
         <Route path="cart/:id" element={<CartPage />} />
+        <Route path="cart" element={<CartPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Route>
     )
   );
-
   return (
     <Box>
       <Nav ref={btnRef} onOpen={onOpen} userInfo={userInfo} />
       <RouterProvider router={router} />
-
-      <AboutUs />
-      <Testimonials />
       <ContactUs />
-
       <Footer />
       <DrawerComponent isOpen={isOpen} onClose={onClose} btnRef={btnRef} />
     </Box>
